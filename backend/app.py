6ccccfda73025bc108e20aa368ca190c04c8ac6d
Respira Automation <automation@respira.cafe>
2025-06-13 19:04:24 -0400

    Remove duplicate CORS header setting

--- backend/app.py
+++ backend/app.py
@@ -35,12 +35,3 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
 
 
-
-@app.after_request
-def after_request(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-    response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-    return response
-
-
